9a41b8e31fd54b33899e88d74f65806811efac46
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-05-31 16:17:40 -0500

    Add a method to call to each array

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,14 @@ friends_ages = [28, 23, 30, 35]
 friends_time = [1.2, 2.4, 4.6, 6.8]
 friends_male = [true, true, true, false]
 
+#This is going to remove "Nate" from the friends varibale
+friends.pop
+
+#This is going to add 44 to the end of my array
+friends_ages.push(44)
+
+#This is going to remove 1.2 from my array
+friends_time.shift
+
+#This is going to add false to the front of my array
+friends_male.unshift(false)
